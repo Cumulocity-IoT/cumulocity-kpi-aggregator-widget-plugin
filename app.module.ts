b044c5c6d5560119070465199dfd5dd7ca876200
Dirk Peter <dirk.peter@softwareg.com>
2024-05-08 14:48:14 +0200

    bump to v0.2

--- app.module.ts
+++ app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BootstrapComponent, CoreModule, RouterModule } from '@c8y/ngx-components';
 import { CockpitDashboardModule } from '@c8y/ngx-components/context-dashboard';
 import { FormlyModule } from '@ngx-formly/core';
+import { KpiAggregatorWidgetPluginModule } from '@widget/kpi-aggregator-widget.module';
 import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { NgChartsModule } from 'ng2-charts';
     CockpitDashboardModule,
     NgChartsModule.forRoot(),
     ReactiveFormsModule,
-    FormlyModule.forRoot()
+    FormlyModule.forRoot(),
+    KpiAggregatorWidgetPluginModule
   ],
   declarations: [],
   bootstrap: [BootstrapComponent]
